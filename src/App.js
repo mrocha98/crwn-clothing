2ddab96bdc1e3578c6e877080bd93da51b181c42
Matheus Rocha <matheus_rocha98@outlook.com>
2019-11-25 10:02:09 -0300

    finishing video71

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
 import {Route, Switch} from 'react-router-dom';
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+
 function App() {
-  return (
-    <div>
-      <Switch>
-        <Route exact path="/" component={HomePage}/>
-        <Route exact path="/shop" component={ShopPage}/>
-      </Switch>
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <Switch>
+                <Route exact path="/" component={HomePage}/>
+                <Route exact path="/shop" component={ShopPage}/>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
